eb0984176096c567801b618df5340b3ab71fa17e
Kanika <you@example.com>
2020-10-13 23:54:39 +0530

    initial code with a simple svg viewport with dots

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let cy = 70;
+  let cx = 70;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <svg height="600" width="500" viewBox="0 0 500 500" >
+          <rect width="600" height="500" fill="lightblue"/>
+          {[...Array(25)].map((key, i) => {
+            let result = <circle cx={cx} cy={cy} r="10" fill="maroon"key={i}/>;
+            cy += 90;
+            if((i+1) % 5 === 0){
+              cy = 70;
+              cx+= 90;
+            }
+            return result;
+            }
+          )}
+        </svg>
       </header>
     </div>
   );

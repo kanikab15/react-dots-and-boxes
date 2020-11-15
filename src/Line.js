9a4c25fc2a3946eb3544c04b4f89a1f0207b11a9
Kanika <you@example.com>
2020-11-15 23:11:02 +0530

    line becomes permanent once clicked

--- src/Line.js
+++ src/Line.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 class Line extends React.Component{
+
+  line = React.createRef();
+
+  handleClick = () => {
+    console.log('click'+this.props.i);
+    this.line.current.classList.add("line-clicked");
+  }
+
   render(){
     return (
-      <line key={'line'+this.props.i} className='line' x1={this.props.x1} y1={this.props.y1} x2={this.props.x2} y2={this.props.y2}
-        onMouseOver={()=> console.log('hover'+this.props.i)} stroke="transparent"
-        strokeWidth="6" onClick={()=> console.log('click'+this.props.i)}/>
+      <line ref={this.line} key={'line'+this.props.i} className='line' x1={this.props.x1} y1={this.props.y1} x2={this.props.x2} y2={this.props.y2}
+        onMouseOver={()=> console.log('hover'+this.props.i)}
+        strokeWidth="6" onClick={this.handleClick}/>
     );
   }
 }

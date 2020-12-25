b425e1287443bada7bf7d50e8a9b1fefe8b44887
Kanika B <kanika.b@gmail.com>
2020-12-25 12:04:03 +0530

    box can keep track of its clicked lines and fill with color when done

--- src/Box.js
+++ src/Box.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import {ClickedContext} from './ClickedContext';
+import {constants} from './constants';
 
 class Box extends React.Component{
+
+  constructor(props){
+    super(props);
+    let areLinesOfBoxActive = props.lineIdsOfBox.reduce((result, key) => ({...result, [key]: false}), {});
+
+    this.state = {
+      areLinesOfBoxActive: areLinesOfBoxActive
+    }
+  }
+
+  componentDidUpdate(){
+    let context = this.context;
+    if(context.state.clickedLineId && !this.state.areLinesOfBoxActive[context.state.clickedLineId]){
+      let areLinesOfBoxActive = this.state.areLinesOfBoxActive;
+      areLinesOfBoxActive[context.state.clickedLineId] = true;
+      this.setState({areLinesOfBoxActive: areLinesOfBoxActive});
+    }
+  }
 
   render(){
     return (
       <ClickedContext.Consumer>
       {(context) => (
-        <g>
-        <polygon key={this.props.i} points={this.props.points} fill="transparent" stroke="white">
+        <polygon key={this.props.i} points={this.props.points}
+        fill={(Object.values(this.state.areLinesOfBoxActive).reduce((result, curr) => {return(result && curr);}, true))? 'blue': 'transparent'}
+        stroke="white">
         </polygon>
-        <text x={this.props.txtPoints.x1} y={this.props.txtPoints.y1} fill="maroon" >
-          {this.props.lineIdsOfBox.find(element => element === context.state.clickedLineId)}</text>
-        </g>
       )}
       </ClickedContext.Consumer>
     );
   }
 }
 
+Box.contextType = ClickedContext;
 export default Box;

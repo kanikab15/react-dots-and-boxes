edc7ad0c09e1613a12a78b99ff5afb5accbbdd48
Kanika B <kanika.b@gmail.com>
2020-12-27 00:42:47 +0530

    !experimental! box can color fill depending on turn but turn not working fully

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Line from './Line';
 import Box from './Box';
 import {ClickedContext} from './ClickedContext';
+import {constants} from './constants';
 
 class App extends React.Component {
   state = {
@@ -10,7 +11,9 @@ class App extends React.Component {
     lineCoordinates: {},
     boxCoordinates: {},
     lineToBoxes: {},
-    clickedLineId:""
+    clickedLineId:"",
+    player1IsNext: true
+    // wasPrevTurnACapture: false
   };
 
   componentDidMount(){
@@ -99,12 +102,14 @@ class App extends React.Component {
         <header className="App-header">
           <svg height="500" width="500" viewBox="0 0 600 600" >
 
-            <circle cx="30" cy="17" r="17" fill="#F29745"/>
-            <rect x="5" y="35" width="50" height="30" rx="10" fill="#F29745"/>
+            <circle cx="30" cy="17" r="17" fill={constants.player1Color}/>
+            <rect x="5" y="35" width="50" height="30" rx="10" fill={constants.player1Color}/>
             <text x="23" y="57" fill="maroon">1</text>
 
-            <circle cx="470" cy="17" r="17" fill="#80BF5E"/>
-            <rect x="445" y="35" width="50" height="30" rx="10" fill="#80BF5E"/>
+            <text x="80" y="57" fill="maroon">{this.state.player1IsNext? 'Player 1 is up': 'Player 2 is up'}</text>
+
+            <circle cx="470" cy="17" r="17" fill={constants.player2Color}/>
+            <rect x="445" y="35" width="50" height="30" rx="10" fill={constants.player2Color}/>
             <text x="465" y="57" fill="maroon">2</text>
 
             <rect y="90" width="500" height="500" fill="lightblue"/>
@@ -118,8 +123,7 @@ class App extends React.Component {
                     points+= `${this.state.boxCoordinates[iNum].x2},${this.state.boxCoordinates[iNum].y2} `;
                     points+= `${this.state.boxCoordinates[iNum].x3},${this.state.boxCoordinates[iNum].y3} `;
                     points+= `${this.state.boxCoordinates[iNum].x4},${this.state.boxCoordinates[iNum].y4} `;
-                    return <Box key={i} points={points} i={i}
-                      txtPoints={{x1:this.state.boxCoordinates[iNum].x1, y1: this.state.boxCoordinates[iNum].y1}}
+                    return <Box key={i} points={points} i={i} player1IsNext={this.state.player1IsNext}
                       lineIdsOfBox={[this.state.boxCoordinates[iNum].line0Id,
                         this.state.boxCoordinates[iNum].line1Id,
                         this.state.boxCoordinates[iNum].line2Id,
--- src/Box.js
+++ src/Box.js
@@ -9,29 +9,40 @@ class Box extends React.Component{
     let areLinesOfBoxActive = props.lineIdsOfBox.reduce((result, key) => ({...result, [key]: false}), {});
 
     this.state = {
-      areLinesOfBoxActive: areLinesOfBoxActive
+      areLinesOfBoxActive: areLinesOfBoxActive,
+      boxCapturedByPlayerColor: ""
     }
   }
 
+  static wasPrevTurnACapture = false;
+
   componentDidUpdate(){
     let context = this.context;
-    if(context.state.clickedLineId && !this.state.areLinesOfBoxActive[context.state.clickedLineId]){
+    let boxCapturedByPlayerColor = "";
+    //process the line click only if it's not already been clicked and exists for this box
+    if(context.state.clickedLineId && (this.state.areLinesOfBoxActive[context.state.clickedLineId] === false)){
       let areLinesOfBoxActive = this.state.areLinesOfBoxActive;
       areLinesOfBoxActive[context.state.clickedLineId] = true;
-      this.setState({areLinesOfBoxActive: areLinesOfBoxActive});
+      let isBoxCaptured =
+        Object.values(areLinesOfBoxActive).reduce((result, curr) => {return(result && curr)}, true);
+      if(isBoxCaptured && !this.state.boxCapturedByPlayerColor)
+        boxCapturedByPlayerColor = (this.props.player1IsNext? constants.player1Color: constants.player2Color);
+      this.setState({ // minimize set states
+        areLinesOfBoxActive: areLinesOfBoxActive,
+        boxCapturedByPlayerColor: boxCapturedByPlayerColor});
+      // this.props.handleTurn(isBoxCaptured, Box.wasPrevTurnACapture);
+      // console.log("before", Box.wasPrevTurnACapture);
+      // Box.wasPrevTurnACapture = isBoxCaptured;
+      // console.log("after", Box.wasPrevTurnACapture);
     }
   }
 
   render(){
     return (
-      <ClickedContext.Consumer>
-      {(context) => (
-        <polygon key={this.props.i} points={this.props.points}
-        fill={(Object.values(this.state.areLinesOfBoxActive).reduce((result, curr) => {return(result && curr);}, true))? 'blue': 'transparent'}
-        stroke="white">
-        </polygon>
-      )}
-      </ClickedContext.Consumer>
+      <polygon key={this.props.i} points={this.props.points}
+      fill={this.state.boxCapturedByPlayerColor? this.state.boxCapturedByPlayerColor: 'transparent'}
+      stroke="white">
+      </polygon>
     );
   }
 }

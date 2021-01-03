2b554953aba363b9d39c959a6dd1a8a831359e5d
Kanika B <kanika.b@gmail.com>
2021-01-04 01:14:55 +0530

    added static content

---
+++ precache-manifest.52436e4aa5234415fe7692dcd8f7e238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d31b08535ef7954d3196cb928e377e18",
+    "url": "/react-dots-and-boxes/index.html"
+  },
+  {
+    "revision": "b729eb18da98d19eb335",
+    "url": "/react-dots-and-boxes/static/css/main.16293302.chunk.css"
+  },
+  {
+    "revision": "cec096b428ece5f2f59a",
+    "url": "/react-dots-and-boxes/static/js/2.afb3d1cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-dots-and-boxes/static/js/2.afb3d1cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b729eb18da98d19eb335",
+    "url": "/react-dots-and-boxes/static/js/main.bb48dd2d.chunk.js"
+  },
+  {
+    "revision": "f0755b5b4e39463d316c",
+    "url": "/react-dots-and-boxes/static/js/runtime-main.4b275c60.js"
+  }
+]);
